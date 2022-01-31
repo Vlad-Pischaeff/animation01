6250589a58a2b1d4299215bc341e59d14b511218
Vlad-Pischaeff <pischaeff@gmail.com>
2022-01-31 15:35:01 +0500

    fix formula...

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import App from './App.svelte';
 
 let target = document.getElementById("animation01");
 
-const app = new App({
-	target: target
-});
+const app = new App({	target });
 
 export default app;
